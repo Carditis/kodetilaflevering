ee0b60d0331e9a1f62d3370cac1c3d80def2617e
Carditis <45385019+Carditis@users.noreply.github.com>
2019-10-30 01:54:13 +0100

    lidt tæt på helt ok
    Virker på de fleste opgaver

---
+++ src/opgave2/SorteretTabel.java
@@ -0,0 +1,106 @@
+package opgave2;
+
+import java.util.Arrays;
+
+public class SorteretTabel {
+
+	int[] array;
+	int a;
+	
+	
+//	public static void main(String[] args) {
+//		int[] a = { 1, 5, 7, 12, 14 };
+//		// int a = 3;
+//		SorteretTabel t = new SorteretTabel(a);
+//		// System.out.print(Arrays.toString(a));
+//
+//	}
+	
+
+	
+	
+	
+	public SorteretTabel(SorteretTabel a, SorteretTabel b) {		
+		int g = a.array.length + b.array.length;
+		array = new int[g];
+		int ia = 0;
+		int ib = 0;
+
+		for(int i = 0; i < g; i++) {
+			if(ia == a.array.length || !(ib == b.array.length || a.array[ia] <= b.array[ib])) {
+				array[i] = b.array[ib++];
+			}else {
+				array[i] = a.array[ia++];
+			}
+		}
+	}
+	public boolean exists(int holger) {
+		int f = array.length;
+		for(int i = 0; i < f; i++) {
+			if(array[i] == holger) {
+				return true;
+			}else if(array[i] > holger) {
+				return false;
+			}
+		}
+		return false;
+	}
+	public int find(int holger) {
+		return helpFinder(holger, 0, array.length);
+	}
+	
+	private int helpFinder(int holger, int start, int end) {
+		while(start < end) {
+			int i = (start + end) / 2;
+			int elem = array[i];
+			if(elem == holger) {
+				return i;
+			}else if(elem < holger) {
+				start = i;
+			}else {
+				end = i;
+			}
+			if (end - start == 1) {
+				return 2;
+			}
+		}
+		return -1;
+//	    if (start >= end)
+//	        return -1;
+//	    int i = (start + end) / 2;
+//	        int elem = array[i];
+//	    if (elem == holger) {
+//	        return i;
+//	    } else if (elem < holger) {
+//	        return helpFinder(holger, start, i);
+//	    } else {
+//	        return helpFinder(holger, i, end);
+//	    }
+	}
+
+	public SorteretTabel(int[] a) {
+		array = a;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] > a[i + 1]) {
+				System.out.println("Fejl");
+				return;
+			}else {
+			} 
+		}
+		//System.out.print(Arrays.toString(a));
+	}
+	public SorteretTabel(int a) {
+		int[] single = {a};
+		this.array = single;
+	}
+
+	public SorteretTabel() {
+		int[] empty = {};
+		this.array = empty;
+	}
+	
+	public String toString(){
+		String s = Arrays.toString(array);
+		return s;
+	}
+}
