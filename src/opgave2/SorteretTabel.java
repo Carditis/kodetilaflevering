7b4b20d830f31964e675f9b185e9d0d725e3ee0b
Carditis <45385019+Carditis@users.noreply.github.com>
2019-11-04 09:03:08 +0100

    nor cormitum

--- src/opgave2/SorteretTabel.java
+++ src/opgave2/SorteretTabel.java
@@ -3,16 +3,23 @@ package opgave2;
 import java.util.Arrays;
 
 public class SorteretTabel {
-
+	
+	//vi starter med at lave nogle variabler
 	int[] array;
 	int a;
-
+	
+	//We make differnet constructors debending on what we want to do with the input. Java knows what constructor to use
+	//for certain input types
 	public SorteretTabel(SorteretTabel a, SorteretTabel b) {
+		//arrays in java have a specifik lenght, so we need to make sure that we create the new array in the right way.
+		//we have used arraylist in python. Arraylist could also be used here, nut decided not to, as we have never used them in java before
+		//We want to add all the numbers from the to difrent tabels, even if they are the same, so we now know the length of the array
 		int g = a.array.length + b.array.length;
 		array = new int[g];
 		int ia = 0;
 		int ib = 0;
-
+		
+		//we run the size of the new array, and use some logic to determine the what number is the biggest.
 		for (int i = 0; i < g; i++) {
 			if (ia == a.array.length || !(ib == b.array.length || a.array[ia] <= b.array[ib])) {
 				array[i] = b.array[ib++];
@@ -23,7 +30,10 @@ public class SorteretTabel {
 	}
 
 	public boolean exists(int holger) {
+		//hehe, we thought we would use the length a bit more, so it is a variable now
+		//We could change it...
 		int f = array.length;
+		//we check every element of the array, and look for Holger
 		for (int i = 0; i <= f-1; i++) {
 			if (array[i] == holger) {
 				return true;
@@ -33,9 +43,11 @@ public class SorteretTabel {
 		}
 		return false;
 	}
-
+	
 	public int find(int holger) {
+		//we first determine if holger even exist
 		if(exists(holger)) {
+			//we call the helping function, with a starting and a end determitor
 			return helpFinder(holger, 0, array.length);
 		}else {
 			return -1;
@@ -44,12 +56,15 @@ public class SorteretTabel {
 	}
 
 	private int helpFinder(int holger, int start, int end) {
+		//we use a while loop, to check if we have reached the end of the array
 		while (start < end) {
+			//we look at the middle of start and end
 			int i = (start + end - 1) / 2;
 			int elem = array[i];
 			if (elem == holger) {
 				return i;
 			} else if (elem < holger) {
+				//thay are sortet, so if holger is bigger then we have a new start location, if less the na new end location
 				start = i;
 			} else {
 				end = i;
@@ -59,30 +74,34 @@ public class SorteretTabel {
 			}
 		}
 		return -1;
+		//we keep doing so, until we find Holger. of till the start is bigger than the end
 	}
 
 	public SorteretTabel(int[] a) {
 		array = a;
+		//we just make sure that what we have, is a sortet tabel
 		for (int i = 0; i < a.length - 1; i++) {
 			if (a[i] > a[i + 1]) {
 				System.out.println("Fejl");
 				return;
-			} else {
 			}
 		}
 	}
 
 	public SorteretTabel(int a) {
+		//making an array with one element
 		int[] single = { a };
 		this.array = single;
 	}
 
 	public SorteretTabel() {
+		//empty array
 		int[] empty = {};
 		this.array = empty;
 	}
 
 	public String toString() {
+		//this function overrides the normal toString in Java, and lets us print the arrays
 		String s = Arrays.toString(array);
 		return s;
 	}
--- src/opgave2/TrianglePattern.java
+++ src/opgave2/TrianglePattern.java
@@ -2,18 +2,22 @@ package opgave2;
 
 public class TrianglePattern {
 	
-
+	//we decided to use a boolean array, as it makes some of the logic easier
 	boolean[][] array;
 	
 	TrianglePattern(int n, int h, int[] initial) {
+		//we teat the gridsize
 		if (testGridSize(n, h)) {
+			//making the 2 dimentional array
 			array = new boolean[h][n];
-
+			
+			//we place all the "seeds", with a simpel for loop
 			for (int i = 0; i < initial.length; i++) {
 				array[0][initial[i]] = true;
 			}
-
+			
 			for (int r = 1; r < array.length; r++) {
+				//we make an above array, that looks at how many trues that are above, and where the are placed
 				boolean[] above = array[r - 1];
 				for (int j = 1; j < above.length - 1; j++) {
 					boolean a = above[j - 1	];
@@ -31,7 +35,8 @@ public class TrianglePattern {
 					if (c) {
 						z = 1;
 					}
-
+					
+					//if there only is one above, or if a is false and b and c is true, then we set it to true
 					boolean one = x + y + z == 1;
 					array[r][j] = one || (!a && b && c);
 				}
@@ -40,12 +45,16 @@ public class TrianglePattern {
 	}
 
 	public boolean testGridSize(int n, int h) {
+		//we test the grid size
 		if (n >= 5) {
 			return true;
 		}
 		return false;
 	}
+	
 	public int getValueAt(int r, int c) {
+		//here we see how cool boolean arrays are. Yes this could be done with other arrays, but this is cool
+		//some would ague that this is cool because booleans don't use as much memory, but we haven't learnt that yet
 		if(array[r][c]) {
 			return 1;
 		}else {
@@ -54,20 +63,24 @@ public class TrianglePattern {
 	}
 	
 	public int getN() {
+		//we can get the lenght of the second dimension, by taking the length of the first row
 		return array[0].length;
 	}
 	
 	public int getH() {
+		//this is the length of the column
 		return array.length;
 	}
 	
 	public int[] getInitial() {
 		int q = 0;
+		//we again need the lenght for the new array we are making
 		for(int i = 0; i < array[0].length; i++) {
 			if(array[0][i]) {
 				q++;
 			}
 		}
+		//we make an array, and go through the first row, to see if it is true, we then at it to the new array
 		int[] initial = new int[q];
 		int s = 0;
 		for(int i = 0; i < array[0].length; i++) {
