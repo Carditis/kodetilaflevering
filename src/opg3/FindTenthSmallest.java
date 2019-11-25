1ebaefdcd404bcf3ef6c0f6b8b00c9efac6e9996
Carditis <45385019+Carditis@users.noreply.github.com>
2019-11-25 17:03:24 +0100

    ser gut

--- src/opg3/FindTenthSmallest.java
+++ src/opg3/FindTenthSmallest.java
@@ -4,18 +4,16 @@ package opg3;
 public class FindTenthSmallest {
 	long[] array;
 
-	public static void main(String[] args) {
-
-	}
-
 	public static long findTenthSmallest(long[] ts) {
         int n = 10;
         long[] array = new long[n];
 
+        //We throw a exception, that stops the program. We do not output a element, if the array is too short.
         if (ts.length < 10) {
-            throw new RuntimeException("Nej tak");
+            throw new RuntimeException("Der er ikke mere end ti elementer, så... du må selv finde det rigtige");
         }
         
+        //We start by initialising an array, with the elements in order
         for (int i = 0; i < n; i++) {
             array[i] = ts[i];
 
@@ -29,6 +27,7 @@ public class FindTenthSmallest {
             }
         }
 
+        //for every element in the original array, we look to see where it would belong in the new sorted array
         for (int i = 10; i < ts.length; i++) {
             for (int j = 10; j > 0; --j) {
                 if (array[j-1] >= ts[i]) {
@@ -44,9 +43,34 @@ public class FindTenthSmallest {
                 }
             }
         }
-
+        //now we can return tenth element, as it is the tenth smallest.
         return array[9];
     }
 }
 
 //new plan, find den som der har 9 der er mindre end.
+ //Det her nede virkede ikke helt, hvilket vi skriver lidt mere om i rapporten
+ /*
+public class FindTenthSmallest {
+	long[] array;
+	
+	public static long findTenthSmallest(long[] ts) {
+		int n = 10;
+		if (ts.length < n) {
+			throw new RuntimeException("Uha, den var ikke helt god");
+		}
+		for (int i = 0; i < ts.length; i++) {
+			int smaller = 0;
+			for (int j = 0; j < ts.length; j++) {
+				if (ts[j] < ts[i]) {
+					smaller++;
+				}
+			}
+			if (smaller == n - 1) {
+				return ts[i];
+			}
+		}
+		return 11111;
+	}
+}
+  */
